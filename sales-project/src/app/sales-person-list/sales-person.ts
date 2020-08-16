15f6ab898b4290a7ccd223c50613627f73ff5b96
Maciek Dom <maciej.rudnik2@gmail.com>
2020-08-16 15:32:19 +0200

    36. Creating a New Angular Component add SalesPerson class

--- sales-project/src/app/sales-person-list/sales-person.ts
+++ sales-project/src/app/sales-person-list/sales-person.ts
@@ -1,2 +1,13 @@
 export class SalesPerson {
+
+
+    constructor(public firstName: string,
+                public lastName: string,
+                public email: string,
+                public salesVolumen: number){
+
+                }
+
+
+                
 }
